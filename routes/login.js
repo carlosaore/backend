70cdc332920256b83e0f5cbff222ccd6512c47c9
Carlos Orellana <carlosaore@gmail.com>
2021-01-28 10:29:50 +0100

    removed files that where ignored but also sent to remote

--- routes/login.js
+++ routes/login.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 
 router.post('/', (req, res) => {
-  console.log(req.body)
-  res.sendStatus(200);
+  passport.authenticate('local', (err, user, info) => {
+      
+  })
 })
+
+// connect to passport to use a local strategy o log in the user
+// if the strategy turns out to work, assign a JWT
+// send this JWT together with user info to the frontend
 
 module.exports = router;
